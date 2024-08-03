82409fac260fc632635200fdfbaff510f19c5974
JakubSchwenkbeck <jakub@schwenkbeck.de>
2024-08-03 15:16:55 +0200

    Init commit

---
+++ app/Main.hs
@@ -0,0 +1,63 @@
+-- Main.hs
+import Graphics.UI.Threepenny
+import Graphics.UI.Threepenny.Core
+
+main :: IO ()
+main = startGUI defaultConfig setup
+
+setup :: Window -> UI ()
+setup window = do
+    return window # set title "Mandelbrot Set"
+    canvas <- UI.canvas
+        # set UI.width 800
+        # set UI.height 600
+        # set style [("border", "1px solid black")]
+    getBody window #+ [element canvas]
+    on UI.click canvas $ \_ -> renderMandelbrot canvas
+
+renderMandelbrot :: Element -> UI ()
+renderMandelbrot canvas = do
+    -- Set up canvas context
+    context <- liftIO $ getCanvasContext canvas
+    let width = 800
+        height = 600
+    liftIO $ setCanvasSize canvas width height
+
+    -- Define Mandelbrot parameters
+    let maxIterations = 100
+        xMin = -2.0
+        xMax = 1.0
+        yMin = -1.5
+        yMax = 1.5
+
+    -- Iterate over pixels and compute Mandelbrot set
+    forM_ [0 .. width - 1] $ \px -> do
+        forM_ [0 .. height - 1] $ \py -> do
+            let x = xMin + (fromIntegral px / fromIntegral width) * (xMax - xMin)
+                y = yMin + (fromIntegral py / fromIntegral height) * (yMax - yMin)
+                iterations = mandelbrotIterations x y maxIterations
+                color = if iterations == maxIterations then "black" else getColor iterations
+            liftIO $ setCanvasColor context px py color
+
+-- Compute Mandelbrot iterations for a given point (x, y)
+mandelbrotIterations :: Double -> Double -> Int -> Int
+mandelbrotIterations x y maxIterations = ...
+    -- Implement Mandelbrot algorithm here
+    -- You can use complex numbers or direct calculations
+    -- For simplicity, I'll leave this part as an exercise for you!
+
+-- Map iterations to colors (you can customize this function)
+getColor :: Int -> String
+getColor iterations = ...
+
+-- Utility function to set canvas color at a specific pixel
+setCanvasColor :: CanvasContext -> Int -> Int -> String -> IO ()
+setCanvasColor context x y color = ...
+    -- Implement setting pixel color here
+    -- You can use context methods like fillRect or similar
+
+-- Utility function to set canvas size
+setCanvasSize :: Element -> Int -> Int -> IO ()
+setCanvasSize canvas w h = ...
+    -- Implement setting canvas size here
+    -- You can use canvas attributes like width and height
